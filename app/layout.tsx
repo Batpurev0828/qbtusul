18ea731f9809d64fa733d151cee6ca912b9b91e4
SpiritSplit <spirit@SpiritSplits-MacBook-Pro.local>
2026-02-24 13:11:00 +0800

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "katex/dist/katex.min.css"
 import { Inter, JetBrains_Mono } from "next/font/google"
 import { AuthProvider } from "@/components/auth-provider"
 import "./globals.css"
+import { Analytics } from "@vercel/analytics/next"
 
 const _inter = Inter({ subsets: ["latin", "cyrillic"] })
 const _jetbrainsMono = JetBrains_Mono({ subsets: ["latin"] })
