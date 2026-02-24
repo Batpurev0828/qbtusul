f93d60775ec460be45c00c24d3e58f42de2e36ef
SpiritSplit <spirit@SpiritSplits-MacBook-Pro.local>
2026-02-24 13:25:49 +0800

    added speed analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter, JetBrains_Mono } from "next/font/google"
 import { AuthProvider } from "@/components/auth-provider"
 import "./globals.css"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 const _inter = Inter({ subsets: ["latin", "cyrillic"] })
 const _jetbrainsMono = JetBrains_Mono({ subsets: ["latin"] })
